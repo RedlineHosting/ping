66b563341adbc5fe215e5572148486352ce5d817
Dylan Ogle <delxhq0@gmail.com>
2023-03-07 14:06:37 +0000

    fix: cors

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
 import { WebSocketServer } from 'ws'
+import cors from 'cors'
 
 const port = process.env.PORT || 9091
 
 const wss = new WebSocketServer({ port })
 
-wss.on('connection', (ws, req) => {
-  ws.on('message', () => {
-    ws.send('Pong')
-    ws.close()
+// Create a middleware function to enable CORS
+const corsMiddleware = cors({
+  origin: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+});
 
-    console.log(`[PING] WS ping from ${req.socket.remoteAddress}`)
+// Use the middleware function for all WebSocket connections
+wss.on('connection', (ws, req) => {
+  // Apply the CORS middleware to the request object
+  corsMiddleware(req, null, () => {
+    ws.on('message', () => {
+      ws.send('Pong')
+      ws.close()
+
+      console.log(`[PING] WS ping from ${req.socket.remoteAddress}`)
+    })
   })
 })
 
